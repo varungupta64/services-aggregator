3df5accaf4813f84afd99a3b14f604280f24e407
vaibhav.bhatnagar <vaibhav.bhtngr03@gmail.com>
2015-11-20 18:59:57 +0530

    Swagger Integration

--- src/main/java/com/exclusively/aggregator/server/AggregatorServer.java
+++ src/main/java/com/exclusively/aggregator/server/AggregatorServer.java
@@ -1,8 +1,14 @@
 package com.exclusively.aggregator.server;
 
+import static com.google.common.collect.Lists.newArrayList;
+import static springfox.documentation.schema.AlternateTypeRules.newRule;
+
 import java.net.InetSocketAddress;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+import org.joda.time.LocalDate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,7 +17,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.ResponseEntity;
 import org.springframework.integration.config.EnableIntegration;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.context.request.async.DeferredResult;
 
 import com.codahale.metrics.MetricRegistry;
 import com.codahale.metrics.graphite.Graphite;
@@ -19,6 +28,22 @@ import com.codahale.metrics.graphite.GraphiteReporter;
 import com.codahale.metrics.graphite.GraphiteSender;
 import com.exclusively.aggregator.controller.AggregationController;
 import com.exclusively.aggregator.services.CatalogAggregatorService;
+import com.fasterxml.classmate.TypeResolver;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseMessageBuilder;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.schema.WildcardType;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.SecurityConfiguration;
+import springfox.documentation.swagger.web.UiConfiguration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * Works as a microservice client, fetching data from the OMS-Service. Uses the
@@ -33,6 +58,7 @@ import com.exclusively.aggregator.services.CatalogAggregatorService;
 @ComponentScan
 @EnableZuulProxy
 @EnableIntegration
+@EnableSwagger2
 public class AggregatorServer {
 
 	public static final String OMS_SERVICE_URL = "http://FLOOR-API";
@@ -66,34 +92,100 @@ public class AggregatorServer {
 	public AggregationController aggregatorController() {
 		return new AggregationController();
 	}
-	
-//	@Bean
-//	public PollScheduler runMonitoring() {
-//		HystrixPlugins.getInstance().registerMetricsPublisher(HystrixServoMetricsPublisher.getInstance());
-//
-//		
-//
-//		// Minimal Servo configuration for publishing to Graphite
-//		final List<MetricObserver> observers = new ArrayList<MetricObserver>();
-//
-//		observers.add(new GraphiteMetricObserver("Test", "10.30.59.201:2003"));
-//		PollScheduler.getInstance().start();
-//		PollRunnable task = new PollRunnable(new MonitorRegistryMetricPoller(), BasicMetricFilter.MATCH_ALL, true, observers);
-//		PollScheduler scheduler = PollScheduler.getInstance();
-//				scheduler.addPoller(task, 5, TimeUnit.SECONDS);
-//		return scheduler;
-//	}
+
+	//	@Bean
+	//	public PollScheduler runMonitoring() {
+	//		HystrixPlugins.getInstance().registerMetricsPublisher(HystrixServoMetricsPublisher.getInstance());
+	//
+	//		
+	//
+	//		// Minimal Servo configuration for publishing to Graphite
+	//		final List<MetricObserver> observers = new ArrayList<MetricObserver>();
+	//
+	//		observers.add(new GraphiteMetricObserver("Test", "10.30.59.201:2003"));
+	//		PollScheduler.getInstance().start();
+	//		PollRunnable task = new PollRunnable(new MonitorRegistryMetricPoller(), BasicMetricFilter.MATCH_ALL, true, observers);
+	//		PollScheduler scheduler = PollScheduler.getInstance();
+	//				scheduler.addPoller(task, 5, TimeUnit.SECONDS);
+	//		return scheduler;
+	//	}
 	@Bean
 	public GraphiteReporter graphiteReporter(MetricRegistry metricRegistry) {
-	    final GraphiteReporter reporter = GraphiteReporter
-	            .forRegistry(metricRegistry)
-	            .build(graphite());
-	    reporter.start(1, TimeUnit.SECONDS);
-	    return reporter;
+		final GraphiteReporter reporter = GraphiteReporter
+				.forRegistry(metricRegistry)
+				.build(graphite());
+		reporter.start(1, TimeUnit.SECONDS);
+		return reporter;
 	}
-	 
+
 	@Bean
 	GraphiteSender graphite() {
-	    return new Graphite(new InetSocketAddress("10.11.19.18", 2003));
+		return new Graphite(new InetSocketAddress("10.11.19.18", 2003));
+	}
+
+	@Bean
+	public Docket petApi() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any())
+				.build()
+				.pathMapping("/")
+				.directModelSubstitute(LocalDate.class,
+						String.class)
+				.genericModelSubstitutes(ResponseEntity.class)
+				.alternateTypeRules(
+						newRule(typeResolver.resolve(DeferredResult.class,
+								typeResolver.resolve(ResponseEntity.class, WildcardType.class)),
+								typeResolver.resolve(WildcardType.class)))
+				.useDefaultResponseMessages(false)
+				.globalResponseMessage(RequestMethod.GET,
+						newArrayList(new ResponseMessageBuilder()
+								.code(500)
+								.message("500 message")
+								.responseModel(new ModelRef("Error"))
+								.build()))
+				.securitySchemes(newArrayList(apiKey()))
+				.securityContexts(newArrayList(securityContext()))
+				//.enableUrlTemplating(true)
+				;
+	}
+
+	@Autowired
+	private TypeResolver typeResolver;
+
+	private ApiKey apiKey() {
+		return new ApiKey("mykey", "api_key", "header");
+	}
+
+	private SecurityContext securityContext() {
+		return SecurityContext.builder()
+				.securityReferences(defaultAuth())
+				.forPaths(PathSelectors.regex("/anyPath.*"))
+				.build();
+	}
+
+	List<SecurityReference> defaultAuth() {
+		AuthorizationScope authorizationScope
+		= new AuthorizationScope("global", "accessEverything");
+		AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+		authorizationScopes[0] = authorizationScope;
+		return newArrayList(
+				new SecurityReference("mykey", authorizationScopes));
+	}
+
+	@Bean
+	SecurityConfiguration security() {
+		return new SecurityConfiguration(
+				"test-app-client-id",
+				"test-app-realm",
+				"test-app",
+				"apiKey");
+	}
+
+	@Bean
+	UiConfiguration uiConfig() {
+		return new UiConfiguration(
+				"validatorUrl");
 	}
 }
